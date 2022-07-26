a69fe0c022be2561e89a9cabc1126844d8d9782a
snaco <jm.anderson.cs@gmail.com>
2022-07-26 17:20:18 -0400

    disable linting in tempalte files

--- src/templates/script_prefix.py
+++ src/templates/script_prefix.py
@@ -1,3 +1,4 @@
+#pylint: disable=all
 query_params = globals()['query_params']
 route = globals()['route']
 body = globals()['body']
--- src/templates/script_suffix.py
+++ src/templates/script_suffix.py
@@ -1,2 +1,3 @@
+#pylint: disable=all
 globals()['response'] = response
 globals()['status'] = status
